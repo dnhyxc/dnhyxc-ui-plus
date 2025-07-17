865a15276c18b357858eb2740cdd20b0469de618
dnhyxc <2640559435@qq.com>
2025-07-17 08:06:30 +0800

    feat: 完善文档

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -7,6 +7,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 export default defineConfig({
   base: '/', // 线上打包路径改为绝对路径，防止打包后，资源文件路径出现上述错误
+  // 为了解决打包 element-plus css 无法处理而报错问题的问题，需要添加以下 ssr 配置
   ssr: {
     noExternal: ['element-plus', 'dnhyxc-ui-plus-beta1']
   },
