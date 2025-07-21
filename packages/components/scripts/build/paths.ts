52770b32fc930e2e6e0820635494672585a41654
dnhyxc <2640559435@qq.com>
2025-07-21 10:35:58 +0800

    feat: 增加version更新插件

--- packages/components/scripts/build/paths.ts
+++ packages/components/scripts/build/paths.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
 
-//组件库根目录
+// 组件库根目录
 export const componentPath = resolve(__dirname, '../../');
 
-//pkg根目录
+// pkg根目录
 export const pkgPath = resolve(__dirname, '../../../');
---
+++ packages/components/scripts/version/index.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { writeFileSync, readFileSync } from 'fs';
+
+// 通过改写__dirname 为__dirnameNew，解决打包报错
+const __filenameNew = fileURLToPath(import.meta.url);
+const __dirnameNew = path.dirname(__filenameNew);
+
+export const getPath = (_path) => path.resolve(__dirnameNew, _path);
+
+const updateVerison = () => {
+  const dnhyxcUIPlusMdPath = getPath('../../../dnhyxc-ui-plus/package.json');
+  const componentsPkgPath = getPath('../../package.json');
+  // 读取并更新 package.json 的版本号
+  const dnhyxcUIPlusPkg = JSON.parse(readFileSync(dnhyxcUIPlusMdPath, 'utf-8'));
+  const componentsPkg = JSON.parse(readFileSync(componentsPkgPath, 'utf-8'));
+
+  componentsPkg.version = dnhyxcUIPlusPkg.version;
+
+  try {
+    writeFileSync(componentsPkgPath, JSON.stringify(componentsPkg, null, 2));
+    console.log(`✨ @dnhyxc-ui/components package.json version 已更新为 ${dnhyxcUIPlusPkg.version}`);
+  } catch (err) {
+    console.error(`package.json 写入错误: ${err.message}`);
+  }
+};
+
+updateVerison();
