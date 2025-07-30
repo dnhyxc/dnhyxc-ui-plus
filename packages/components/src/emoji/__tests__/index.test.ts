b070becea8581cb0915a2839061704078d25f35c
dnhyxc <2640559435@qq.com>
2025-07-30 14:25:03 +0800

    feat: 增加 vitest 脚本

--- packages/components/src/emoji/__tests__/index.test.ts
+++ packages/components/src/emoji/__tests__/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
-import Emoji from '../../emoji';
+import Emoji from '..';
 
 describe('Emoji Component', () => {
   // 测试基础渲染
